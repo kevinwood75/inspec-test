5602fe8575a61f02f91636ee5558b9189f9dbc15
kevinwood75 <kevin.wood75@gmail.com>
2018-12-09 15:59:25 -0500

    fixed inspec check

--- controls/grub-check.rb
+++ controls/grub-check.rb
@@ -1,7 +1,7 @@
 control "cis-1-5-1" do
   impact 1.0
-  title "check grub.conf"
-  desc "check grub.conf"
+  title "grub"
+  desc "grub"
   describe file('/etc/grub2.cfg') do
     its('owner') { should eq 'root' }
     its('group') { should eq 'root' }
