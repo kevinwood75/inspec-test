0a1e7f2bbbf477d4d3b6294b95d0047adb8b6e4a
kevinwood75 <kevin.wood75@gmail.com>
2019-02-10 00:26:13 -0500

    added tags

--- controls/tmp-check.rb
+++ controls/tmp-check.rb
@@ -2,6 +2,8 @@ control 'tmp' do
   impact 0.7
   title 'tmp'
   desc "tmp"
+  tag category: 'security'
+  tag technology: 'rhel'
   describe file ('/tmp') do
     it { should be_directory }
     its('owner') { should eq 'root' }
