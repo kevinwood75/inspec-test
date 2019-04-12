b153bf5ebcd4a04cb4bf908406d42ffeb6cbdc2b
kevinwood75 <kevin.wood75@gmail.com>
2019-04-12 00:05:01 -0400

    added baseline check

--- controls/cpu.rb
+++
@@ -1,9 +0,0 @@
-control "cpu" do
-  impact 1.0
-  title 'cpu'
-  desc "cpu"
-
-  describe file('/proc/cpuinfo') do
-    its('size') { should be <= 120 }
-  end
-end
--- controls/gpg-activated.rb
+++
@@ -1,8 +0,0 @@
-control "gpgcheck" do
-  impact 1.0
-  title "gpgcheck"
-  desc "gpgcheck"
-  describe file('/etc/yum.conf') do
-    its('content') { should match /gpgcheck=1/ }
-  end
-end
